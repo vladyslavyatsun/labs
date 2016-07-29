4d6e0334d592b90ae4a732e474eb7282ef841dbd
challenger <challenger@zeouniversity.com>
2016-07-29 15:25:26 +0300

    init

---
+++ testProject/testProject/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  testProject
+//
+//  Created by Владислав Яцун on 7/5/16.
+//  Copyright © 2016 challenger. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
+
+
+void meth(int a){
+    printf("it works");
+}
